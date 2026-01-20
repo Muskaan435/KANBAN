82043bd80afd815796644b03502b104bb11e5bba
Muskaan435 <muskaanpichode@gmail.com>
2026-01-20 21:18:40 +0530

    included add new task button working features

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const done = document.querySelector('#done');
 let draggedElement = null;
 const toggleModalButton =document.querySelector("#toggle-modal");
 const modal = document.querySelector(".modal");
-const modalBg = document.querySelector(".bg")
+const modalBg = document.querySelector(".bg");
+const addTaskButton = document.querySelector("#add-new-button");
 
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(tasks => {
-    tasks.addEventListener("drag", (e) => {
+    tasks.addEventListener("dragstart", (e) => {
         draggedElement = tasks;
     })
 })
@@ -33,14 +34,36 @@ function addDragEvent(column){
 
     })
 }
-//add task logic
+//add task button logic
 toggleModalButton.addEventListener("click", ()=>{
     modal.classList.toggle("active");
 } )
-//add task logic
+
 modalBg.addEventListener("click" ,()=>{
     modal.classList.remove("active");
 })
+
+addTaskButton.addEventListener("click", ()=>{
+    const TaskTitle = document.querySelector("#task-title-input").value;
+    const TaskDescription = document.querySelector("#task-description-input").value;
+    
+    const div = document.createElement("div");
+
+    div.classList.add("task");
+    div.setAttribute("draggable","true");
+    div.innerHTML=`
+        <h2>${TaskTitle}</h2>
+        <p>${TaskDescription}</p>
+        <button id="delete-button">Delete</button>
+    `;
+    todo.appendChild(div);
+    modal.classList.remove("active");
+
+    div.addEventListener("dragstart", (e)=>{
+        draggedElement = div;
+    } )
+})
+//add task button logic
 addDragEvent(todo);
 addDragEvent(progress);
 addDragEvent(done);
